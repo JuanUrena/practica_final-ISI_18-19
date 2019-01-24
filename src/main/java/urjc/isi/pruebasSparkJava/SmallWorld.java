b675c0234ddb354db3af155cfdd71d8085534ec0
Onac8 <canopicazojonathan@gmail.com>
2019-01-24 13:10:27 +0100

    Añadida funcionalidad: información del grafo, uso de IndexGraph.java

--- src/main/java/urjc/isi/pruebasSparkJava/SmallWorld.java
+++ src/main/java/urjc/isi/pruebasSparkJava/SmallWorld.java
@@ -32,6 +32,16 @@ public class SmallWorld {
         }
         return (double) sum / (G.V() * (G.V() - 1));
     }
+    
+    // Nuevo método. Dado un nodo v, devolvemos su maxPathLength
+//    public static int pathLength(Graph G, String v) {
+//        int sum = 0;
+//        PathFinder pf = new PathFinder(G, v);
+//        for (String w : G.vertices()){
+//                sum += pf.distanceTo(w);
+//        }
+//        return sum;
+//    }
 
 
     // Compute clustering coefficient.
