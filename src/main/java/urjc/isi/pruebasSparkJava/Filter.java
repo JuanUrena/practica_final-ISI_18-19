2bff269990fad59473a8b952709cc79574f975bc
malozanom <ma.lozanom@alumnos.urjc.es>
2019-01-24 01:09:20 +0100

    Creado esqueleto Filter.java

---
+++ src/main/java/urjc/isi/pruebasSparkJava/Filter.java
@@ -0,0 +1,26 @@
+package urjc.isi.pruebasSparkJava;
+
+//Clase encargada de todo lo relacionado con el filtrado.
+
+public class Filter {
+
+	// Método encargado de mostrar al usuario el menú con las distintas
+	// opciones para filtrar (HTML devuelto al hacer GET sobre /filter)
+	public static String showFilterMenu() {
+
+	}
+
+	// Método encargado de mostrar al usuario toda la información sobre
+	// la película cuyo nombre ha introducido (HTML devuelto al hacer
+	// POST sobre /filter_name)
+	public static String showFilmByName() {
+
+	}
+	
+	// Método encargado de mostrar al usuario todas las películas estrenadas
+	// en el año que ha introducido (HTML devuelto al hacer POST sobre /filter_year)
+	public static String showFilmByYear() {
+		
+	}
+	
+}
