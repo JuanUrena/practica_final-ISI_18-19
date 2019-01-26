1fd0a8e9b7b2fd59c86011fb1a8dda6a706a0e6e
malozanom <ma.lozanom@alumnos.urjc.es>
2019-01-26 16:56:44 +0100

    Quitado obtención de campo 'titleID' al devolver info película

--- src/main/java/urjc/isi/pruebasSparkJava/Injector.java
+++ src/main/java/urjc/isi/pruebasSparkJava/Injector.java
@@ -37,14 +37,12 @@ public class Injector {
     		ResultSet rs = pstmt.executeQuery();
     		c.commit();
     		rs.next();
-            String titleid = Integer.toString(rs.getInt("titleID"));
             String title = rs.getString("title");
             String year = Integer.toString(rs.getInt("year"));
             String runtimeMinutes = Integer.toString(rs.getInt("runtimeMinutes"));
             String averageRating = Double.toString(rs.getDouble("averageRating"));
             String numVotes = Integer.toString(rs.getInt("numVotes"));
             String genres = rs.getString("genres");
-            result.add(titleid);
             result.add(title);
             result.add(year);
             result.add(runtimeMinutes);
