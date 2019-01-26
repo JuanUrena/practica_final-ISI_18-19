4db84eaba8fbcd622aa2adacbc9dd4daeb38de0e
LydiaGarrido <l.garridomu@alumnos.urjc.es>
2019-01-26 12:23:28 +0100

    Arreglados más fallos MainTest.java

--- src/test/java/urjc/isi/pruebasSparkJava/MainTest.java
+++ src/test/java/urjc/isi/pruebasSparkJava/MainTest.java
@@ -1,8 +1,17 @@
 package urjc.isi.pruebasSparkJava;
 
 import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.*;
 import java.util.*;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class MainTest {
 	
@@ -13,7 +22,6 @@ public class MainTest {
 		connection = null;
     		try {
       			connection = DriverManager.getConnection("jdbc:sqlite:Database/IMDb.db");
-      			Bbdd.eraseBBDD(connection); // Prepare SQL to create table
     		} catch(SQLException e) {
 	      		System.err.println(e.getMessage());
 	    		}
@@ -33,20 +41,20 @@ public class MainTest {
    	 	}
 	}
 
-	@Test //(expected = NullPointerException.class)
+	@Test (expected = NullPointerException.class)
 	public void testInsertNullMovie()
 	{
 		Main.insertFilm(connection, null, "2019", null);
 	}
 
-	@Test (expected=NullPointerException.class)
+	@Test (expected = NullPointerException.class)
 	public void testInsertNullActor() throws SQLException {
 		Main.insertActor(connection, null);
 	}
 
 	
-	@Test (expected=NullPointerException.class)
-  	public void testInsertNullWorks() throws 	SQLException {
-    	Main.insertWorks_In(connection, null, null);
+	@Test (expected = NullPointerException.class)
+  	public void testInsertNullWorks() throws SQLException {
+    		Main.insertWorks_In(connection, null, null);
 	}	
 }
