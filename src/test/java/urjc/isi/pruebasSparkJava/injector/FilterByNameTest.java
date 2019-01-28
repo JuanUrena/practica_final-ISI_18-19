862ac4f12a06a5f61daab4097b78969c92ba3789
malozanom <ma.lozanom@alumnos.urjc.es>
2019-01-28 20:24:33 +0100

    Creado FilterByNameTest.java

---
+++ src/test/java/urjc/isi/pruebasSparkJava/injector/FilterByNameTest.java
@@ -0,0 +1,13 @@
+package urjc.isi.pruebasSparkJava.injector;
+
+import static org.junit.Assert.*;
+import org.junit.*;
+import java.util.*;
+import urjc.isi.pruebasSparkJava.Injector;
+
+public class FilterByNameTest {
+
+	Injector connector = new Injector("JDBC_DATABASE_URL");		// Test fixture
+	private List<String> expectedResults;
+	
+}
