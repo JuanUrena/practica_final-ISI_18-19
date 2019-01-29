0b3b69a2ff85a357bf9a7a480b7805295ed1bff4
Alberto Rafael Rodríguez Iglesias <albertocysec@gmail.com>
2019-01-29 21:25:56 +0100

    MainTest.java readded

---
+++ src/test/java/urjc/isi/pruebasSparkJava/MainTest.java
@@ -0,0 +1,27 @@
+package urjc.isi.pruebasSparkJava;
+
+import static org.junit.Assert.*;
+import org.junit.*;
+import java.util.*;
+
+public class MainTest {
+
+	private String s;
+
+	@Before
+	public void setUp()
+	{
+		;
+	}
+
+	@After
+	public void tearDown()
+	{
+		;
+	}
+
+	@Test
+	public void testExample(){
+		;
+	}
+}
