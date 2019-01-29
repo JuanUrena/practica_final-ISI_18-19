654ce4aef1d3adc70df9603a8316910863f08234
Alberto Rafael Rodríguez Iglesias <albertocysec@gmail.com>
2019-01-29 21:50:11 +0100

    Tries on testing files

---
+++ src/test/java/urjc/isi/pruebasSparkJava/SlopeOneFilter/PredictTest.java
@@ -0,0 +1,25 @@
+package urjc.isi.pruebasSparkJava;
+
+import static org.junit.Assert.*;
+import org.junit.*;
+import java.util.*;
+import urjc.isi.pruebasSparkJava.SlopeOneFilter;
+
+public class PredictTest {
+
+	private String s;
+
+
+// BLACK BOX TESTS
+
+	@Test
+	public void happyPath(){
+		System.out.println("...");
+	}
+
+// WHITE BOX TESTS
+
+
+
+
+}
