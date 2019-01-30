637db034f1086f6d84a31f23a205297422d1700a
villalba5 <44021441+villalba5@users.noreply.github.com>
2019-01-30 01:48:07 +0100

    modificado FilterByRatingTest.java

--- src/test/java/urjc/isi/pruebasSparkJava/injector/FilterByRatingTest.java
+++ src/test/java/urjc/isi/pruebasSparkJava/injector/FilterByRatingTest.java
@@ -10,12 +10,21 @@ public class FilterByRatingTest {
 	// Test fixture
 	Injector connector = new Injector("JDBC_DATABASE_URL");
 	private String rating;
+	//Test 1: PAra probar que funciona el método filterByRating, lo que voy a hacer es pasarle como argumento un 0.1
+	// y me tendrá que devolver un ArrayList de mas de 1 posición
+	@Test
+	public void test1() {
+		rating = "0.1";
+		List<String> movies = connector.filterByRating(rating);
+		
+		assertTrue(movies.size()>0);
+	
 	
 	// Test 2: A2 = F => no hay películas con una valoración mayor en la BD.
 	// Lo comprobamos con "10.1"
 	// Recorre el camino de prueba i = [1, 2, 4, 5, 6, 9] 
 	@Test
-	public void test1() {
+	public void test2() {
 		rating = "10.1";
 		List<String> movies = connector.filterByRating(rating);
 		
